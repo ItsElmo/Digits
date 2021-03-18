d7c25a5c199de099b406d314a9a0d1aa1d9fc6ad
Patrick <patrickgcauley@gmail.com>
2021-03-17 21:23:40 -0400

    init

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import express from 'express';
+import { createConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'graphql';
+import { HelloResolver } from './resolvers/HelloResolver';
+
+(async () => {
+	const app = express();
+	app.get('/', (_req, res) => res.send('hello'));
+
+	await createConnection();
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver]
+		}),
+		context: ({ req, res }) => ({ req, res }),
+	});
+	apolloServer.applyMiddleware({ app });
+	app.listen(4000, () => {
+		console.log('express ting started');
+	});
+})();
